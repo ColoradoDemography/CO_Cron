3d6133287fa5b8f05a52242eb3ac8a887b565dbf
Daniel Trone <royhobbstn@yahoo.com>
2016-03-22 17:00:51 +0000

    ugh wrong username

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function getTimeStamp() {
 var thisrun=getTimeStamp();
 
 //pg_dump test
-exec('pg_dump -Fc -h 104.197.26.248 -U codemog -w -p 5433 -d dola > db/dola' + thisrun + '.custom', {}, function (error, stdout, stderr) {
+exec('pg_dump -Fc -h 104.197.26.248 -U postgres -w -p 5433 -d dola > db/dola' + thisrun + '.custom', {}, function (error, stdout, stderr) {
     console.log('--db-dump--');
     console.log('error: ' + error); console.log('stdout: ' + stdout); console.log('stderr: ' + stderr);
     db_bucket.upload('db/dola' + thisrun + '.custom', function(err, file) {if (!err) { console.log('success uploading db/dola' + thisrun + '.custom'); } else {console.log(err); } });
