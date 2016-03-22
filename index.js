d908b8ca8cd71e4e6785d3b02080050f732df4ab
Daniel Trone <royhobbstn@yahoo.com>
2016-03-22 15:31:10 +0000

    db dump test

--- index.js
+++ index.js
@@ -12,25 +12,32 @@ var gcs = gcloud.storage({
   keyFilename: 'key/dola-gis-server-f143cd56dce3.json'
 });
 
-var bucket = gcs.bucket('co-publicdata');
-
-bucket.acl.add({
-  entity: 'allUsers',
-  role: gcs.acl.READER_ROLE
-}, function(err, aclObject) {});
-
+var data_bucket = gcs.bucket('co-publicdata');
+var db_bucket = gcs.bucket('dola-db-dump');
 
 mkdirp('data', function(err) { 
     console.log('data folder created');
 });
+mkdirp('db', function(err) { 
+    console.log('db folder created');
+});
+
+//pg_dump test
+exec("pg_dump -Fc -h 104.197.26.248 -U postgres -p 5432 -d dola > db/dola.custom", {}, function (error, stdout, stderr) {
+    console.log('--db-dump--');
+    console.log('error: ' + error); console.log('stdout: ' + stdout); console.log('stderr: ' + stderr);
+    db_bucket.upload('db/dola.custom', function(err, file) {if (!err) { console.log('success uploading data/dlmetro.zip'); } else {console.log(err); } });
+});
+
 
 //metro districts
 var a = schedule.scheduleJob('* * * * *', function(){
     var command="pgsql2shp -f data/dlmetro -h 54.69.15.55 -u codemog -P demography dola \"select lgid,source,geom,lgname,lgtypeid,lgstatusid,abbrev_name,mail_address,alt_address,mail_city,mail_state,mail_zip,url,prev_name from dola.bounds.districts natural join dola.bounds.lgbasic where lgtypeid='6';\" ";
     exec(command, {}, function (error, stdout, stderr) {
+    console.log('--metro--');
     console.log('error: ' + error); console.log('stdout: ' + stdout); console.log('stderr: ' + stderr);
     execSync("zip -o data/dlmetro.zip data/dlmetro.dbf data/dlmetro.prj data/dlmetro.shp data/dlmetro.shx");
-    bucket.upload('data/dlmetro.zip', function(err, file) {if (!err) { console.log('success with data/dlmetro.zip'); } else {console.log(err); } });
+    data_bucket.upload('data/dlmetro.zip', function(err, file) {if (!err) { console.log('success uploading data/dlmetro.zip'); } else {console.log(err); } });
     });
 });  
 
@@ -39,9 +46,10 @@ var a = schedule.scheduleJob('* * * * *', function(){
 var b = schedule.scheduleJob('* * * * *', function(){
     var command="pgsql2shp -f data/dlpark -h 54.69.15.55 -u codemog -P demography dola \"select lgid,source,geom,lgname,lgtypeid,lgstatusid,abbrev_name,mail_address,alt_address,mail_city,mail_state,mail_zip,url,prev_name from dola.bounds.districts natural join dola.bounds.lgbasic where lgtypeid='7';\" ";
     exec(command, {}, function (error, stdout, stderr) {
+    console.log('--park--');
     console.log('error: ' + error); console.log('stdout: ' + stdout); console.log('stderr: ' + stderr);
     execSync("zip -o data/dlpark.zip data/dlpark.dbf data/dlpark.prj data/dlpark.shp data/dlpark.shx;");  
-    bucket.upload('data/dlpark.zip', function(err, file) {if (!err) { console.log('success with data/dlpark.zip'); } else {console.log(err); } });      
+    data_bucket.upload('data/dlpark.zip', function(err, file) {if (!err) { console.log('success uploading data/dlpark.zip'); } else {console.log(err); } });      
     });
 });
 /*
