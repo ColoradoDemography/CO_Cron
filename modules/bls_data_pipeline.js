64a203589dc2fb7011eb7ffee910a315c6fdf9e2
Brian Timoney <brian@thetimoneygroup.com>
2016-08-22 09:25:14 -0600

    putting BLS data in BLS bucket
    sync-ing up reference with index.js

--- modules/bls_data_pipeline.js
+++ modules/bls_data_pipeline.js
@@ -4,14 +4,14 @@
 var exec = require('child_process').exec;
 
 
-module.exports = function(data_bucket) {
+module.exports = function(bls_bucket) {
     var command = "php bls/bls.php";
     exec(command, {}, function(error, stdout, stderr) {
         console.log('--bls--');
         console.log('error: ' + error);
         console.log('stdout: ' + stdout);
         console.log('stderr: ' + stderr);
-        data_bucket.upload('json/08_bls.json', function(err, file) {
+        bls_bucket.upload('json/08_bls.json', function(err, file) {
             if (!err) {
                 console.log('success uploading json/08_bls.json');
             } else {
