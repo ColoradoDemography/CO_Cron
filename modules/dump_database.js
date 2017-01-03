21753e2fea7866dd47351e844d62411ef9c4c41f
TecmoB <todd.bleess@gmail.com>
2017-01-03 08:21:28 -0700

    Using IP address for postgres
    Changing gis.dola.colorado.gov to the IP address to try making the dump work.

--- modules/dump_database.js
+++ modules/dump_database.js
@@ -6,7 +6,7 @@ var getTimeStamp = require('./get_timestamp.js');
 
 module.exports = function(db_bucket) {
     var thisrun = getTimeStamp();
-    exec('pg_dump -Fc -h gis.dola.colorado.gov -U postgres -w -p 5433 -d dola > db/dola' + thisrun + '.custom', {}, function(error, stdout, stderr) {
+    exec('pg_dump -Fc -h 104.197.26.248 -U postgres -w -p 5433 -d dola > db/dola' + thisrun + '.custom', {}, function(error, stdout, stderr) {
         console.log('--db-dump--');
         console.log('error: ' + error);
         console.log('stdout: ' + stdout);
