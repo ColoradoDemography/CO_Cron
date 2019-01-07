433b180c9e0fac2b8b32088e5638ae216f4ba05b
TBleess <todd.bleess@gmail.com>
2019-01-07 13:40:46 -0700

    More SAR Types

--- test_grant_export_files.js
+++ test_grant_export_files.js
@@ -2,12 +2,15 @@
 
 var grants_export = require('./modules/grants_export.js');
 
-grants_export('FML,SEV_DIST,VFP,CTF,SAR,FFB,EIAF,GAME,REDI,DR,CSBG,CDBG');
+grants_export('FML,SEV_DIST,VFP,CTF,SAR,SAR EoY,SAR Tier 1,SAR Tier 3,FFB,EIAF,GAME,REDI,DR,CSBG,CDBG');
 grants_export('FML');
 grants_export('SEV_DIST');
 grants_export('VFP');
 grants_export('CTF');
 grants_export('SAR');
+grants_export('SAR EoY');
+grants_export('SAR Tier 1');
+grants_export('SAR Tier 3');
 grants_export('FFB');
 grants_export('EIAF');
 grants_export('GAME');
