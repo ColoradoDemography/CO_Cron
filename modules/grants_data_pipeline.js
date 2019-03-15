6fa54eca775a70c57f22d2f3a8977b2d29d19b34
TBleess <todd.bleess@gmail.com>
2019-03-15 15:25:43 -0600

    grants php

--- modules/grants_data_pipeline.js
+++ modules/grants_data_pipeline.js
@@ -30,3 +30,71 @@ module.exports.fsgrants = function(data_bucket) {
         console.log('stderr: ' + stderr);
     });
 }
+
+module.exports.competitive = function(bls_bucket) {
+    var command = "php https://storage.cloud.google.com/bls-data/get_competitive.php";
+    exec(command, {}, function(error, stdout, stderr) {
+        console.log('--fs-grants--');
+        console.log('error: ' + error);
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        data_bucket.upload('competivitive2.json', function(err, file) {
+            if (!err) {
+                console.log('success uploading competitive2.json');
+            } else {
+                console.log(err);
+            }
+        });
+    });
+}
+
+module.exports.allcompetitive = function(bls_bucket) {
+    var command = "php https://storage.cloud.google.com/bls-data/get_competitive_all.php";
+    exec(command, {}, function(error, stdout, stderr) {
+        console.log('--fs-grants--');
+        console.log('error: ' + error);
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        data_bucket.upload('allcompetivitive.json', function(err, file) {
+            if (!err) {
+                console.log('success uploading allcompetitive.json');
+            } else {
+                console.log(err);
+            }
+        });
+    });
+}
+
+module.exports.formulaic = function(bls_bucket) {
+    var command = "php https://storage.cloud.google.com/bls-data/get_formulaic.php";
+    exec(command, {}, function(error, stdout, stderr) {
+        console.log('--fs-grants--');
+        console.log('error: ' + error);
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        data_bucket.upload('formulaic.json', function(err, file) {
+            if (!err) {
+                console.log('success uploading formulaic.json');
+            } else {
+                console.log(err);
+            }
+        });
+    });
+}
+
+module.exports.allformulaic = function(bls_bucket) {
+    var command = "php https://storage.cloud.google.com/bls-data/get_formulaic_all.php";
+    exec(command, {}, function(error, stdout, stderr) {
+        console.log('--fs-grants--');
+        console.log('error: ' + error);
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+        data_bucket.upload('allformulaic.json', function(err, file) {
+            if (!err) {
+                console.log('success uploading allformulaic.json');
+            } else {
+                console.log(err);
+            }
+        });
+    });
+}
