903389faacd2fb509340bb68c5e6c379680d73ad
TBleess <todd.bleess@state.co.us>
2021-07-16 13:04:01 -0600

    Update load_lginfo.php

--- php/load_lginfo.php
+++ php/load_lginfo.php
@@ -16,7 +16,7 @@ $credentials = "user=postgres";
 
 //require "/root/pg.php";
 
-$str = file_get_contents('https://dola.colorado.gov/gis-tmp/lginfo.json');
+$str = file_get_contents('https://storage.googleapis.com/co-publicdata/lginfo.json');
 
 $json = json_decode($str, true); // decode the JSON into an associative array
 
